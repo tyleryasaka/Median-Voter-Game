210f0957bf3b557342cca73b06c4487c3f3d2948
Tyler Yasaka <contact@tyleryasaka.me>
2017-03-08 03:46:02 -0500

    Got somethin' workin'

---
+++ src/App.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react';
+import './App.css';
+import Voters from './Voters.js';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      candidates: [25, 50, 75],
+      max: 100
+    };
+  }
+
+  sortedCandidates(arr) {
+    return arr.slice(0).sort((a, b) => {
+      return a - b;
+    });
+  }
+
+  halfBetween(a, b) {
+    return (a + b) / 2;
+  }
+
+  getPrevUnique(arr, index, value) {
+    let prevIndex = index - 1;
+    while(prevIndex >= 0 && arr[prevIndex] === value) {
+      prevIndex--;
+    }
+    return prevIndex >= 0 ? arr[prevIndex] : null;
+  }
+
+  getNextUnique(arr, index, value) {
+    let lastIndex = (arr.length - 1);
+    let nextIndex = index + 1;
+    while(nextIndex <= lastIndex && arr[nextIndex] === value) {
+      nextIndex++;
+    }
+    return nextIndex <= lastIndex ? arr[nextIndex] : null;
+  }
+
+  countOccurences(arr, value) {
+    return arr.filter(item => {
+      return item === value;
+    }).length;
+  }
+
+  isDominantStrategy(candidates, currentIndex, testValue) {
+    let bestOutcome = 0;
+    let bestOut = null;
+    let bestIndex = -1;
+    let testCandidates = candidates.slice(0);
+    for(var i = 0; i < this.state.max; i++) {
+      testCandidates[currentIndex] = i;
+      let anotherOutcome = this.collectVotes(testCandidates, i);
+      if(anotherOutcome.individualVotes > bestOutcome) {
+        bestOutcome = anotherOutcome.individualVotes;
+        bestOut = anotherOutcome;
+        bestIndex = i;
+      }
+    }
+    return bestOutcome === testValue;
+  }
+
+  collectVotes(candidates, value) {
+    let start;
+    let end;
+    let sorted = this.sortedCandidates(candidates);
+    let tied = this.countOccurences(sorted, value);
+    let max = this.state.max;
+    let individualVotes;
+    let sharedVotes;
+    sorted.forEach((candidate, index) => {
+      if(candidate === value) {
+        let prev = this.getPrevUnique(sorted, index, value);
+        let next = this.getNextUnique(sorted, index, value);
+        start = prev !== null ? this.halfBetween(prev, candidate) : 0;
+        end = next !== null ? this.halfBetween(candidate, next) : max;
+      }
+    });
+    sharedVotes = end - start;
+    individualVotes = tied ? (sharedVotes / tied) : 1;
+
+    return {start, sharedVotes, individualVotes, tied};
+  }
+
+  changePosition(index, event) {
+    let candidates = this.state.candidates;
+    candidates[index] = Number(event.target.value);
+    this.setState({candidates});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.candidates.map((candidate, index) => {
+            let result = this.collectVotes(this.state.candidates, candidate);
+            let start = result.start;
+            let votes = result.sharedVotes;
+            let height = result.tied ? 1 / result.tied : 1;
+            let max = this.state.max;
+            let isDominantStrategy = this.isDominantStrategy(this.state.candidates, index, result.individualVotes);
+            return (<Voters key={index} index={index} position={candidate} start={start} votes={votes} isDominantStrategy={isDominantStrategy} height={height} max={max} step="" onChange={this.changePosition.bind(this)} />);
+          })
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Voters.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+const barHeight = 50;
+
+class Voters extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    let style = {
+      width: String(this.props.votes) + "%",
+      height: String(this.props.height * barHeight) + "px",
+      marginLeft: String(this.props.start) + "%"
+    };
+    let max = this.props.max;
+    let step = this.props.step;
+    let votes = Math.round(this.props.votes);
+    let isBestStrategy = String(this.props.isDominantStrategy);
+    return (
+      <div>
+        <div className="all-voters">
+          <div className="selected-voters" style={style}></div>
+        </div>
+        <input type="range" defaultValue={this.props.position} max={max} step={step} onChange={this.props.onChange.bind(this, this.props.index)}/>
+        <div className="position-display">
+          Position: {this.props.position}<br />
+          Votes: {votes}%<br />
+          Best strategy: {isBestStrategy}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Voters;
